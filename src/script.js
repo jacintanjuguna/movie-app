3d0dec0a6f53d11191a432aa2fde8741d9fc2812
Jess <102421883+jacintanjuguna@users.noreply.github.com>
2022-06-24 09:19:41 +0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,58 @@
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+
+getMovie(APIURL);
+
+function getMovie(url) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => showMovie(data.results))
+}
+
+function showMovie(movies) {
+    //clear main
+    main.innerHTML = '';
+    movies.forEach((movie) => {
+        const { poster_path, title, overview, release_date, vote_average } = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `  
+        <img 
+        src="${IMGPATH}${movie.poster_path}"" 
+        alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span id="col-shrunk" class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        `;
+
+        main.appendChild(movieEl);
+    })
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return 'green';
+    } else if (vote >= 6) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const searchTerm = e.target.search.value;
+    if (searchTerm) {
+        getMovie(SEARCHAPI + searchTerm);
+        search.value = "";
+    }
+
+})
